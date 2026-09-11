aef39046c3ba77d2a4d995de2643ab931b9c328a
ObsidianMultiSync <sync@obsidian.local>
2026-09-11 19:36:29 +0500

    sync: 2026-09-11 14:36:29

---
+++ Дисер/code/core/context.py
@@ -0,0 +1,14 @@
+from typing import Dict
+
+import torch
+
+
+class Context:
+    declarations: Dict[str, Declaration]
+
+    def get_declaration(self, name: str) -> int | float | torch.tensor:
+        return self.declarations[name].value.eval(self)
+
+    def set_declaration(self, declaration: Declaration) -> None:
+        self.declarations[declaration.name] = declaration
+
